f8fc263dd99efbdf8b7339f1e082f4888dfb52b9
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2013-10-13 23:06:30 +0100

    Made the colour scheme change when changing sections.

--- web/scripts/main.js
+++ web/scripts/main.js
@@ -1,3 +1,50 @@
+ColourScheme = {
+    DARK_PURPLE: {
+        text: "#fff",
+        body: "#113",
+        sectionBody: "#335"
+    },
+    DARK_RED: {
+        text: "#fff",
+        body: "#311",
+        sectionBody: "#533"
+    }
+};
+
+function pickColourScheme() {
+
+    var colourSchemeNum   = _.size(ColourScheme);
+    var colourSchemeIndex = _.random(colourSchemeNum - 1);
+    var colourSchemeKey   = _.keys(ColourScheme)[colourSchemeIndex];
+
+    return ColourScheme[colourSchemeKey];
+
+}
+
+function setColourScheme(currentColourScheme) {
+
+    var newColourScheme = pickColourScheme();
+
+    while(currentColourScheme === newColourScheme) {
+        newColourScheme = pickColourScheme();
+    }
+
+    $("body").stop().animate({
+        "background-color": newColourScheme.body,
+        "color": newColourScheme.text
+    });
+
+    // FIXME: Animate this transition with the accordion.
+    $(".section-body").each(function() {
+        $(this).css({
+            "background-color": newColourScheme.sectionBody
+        });
+    });
+
+    return newColourScheme;
+
+}
+
 function setAllSectionBodyMaxHeights(minHeight, isOnResize) {
 
     var bodyOverflowYValue   = "hidden";
@@ -27,12 +74,44 @@ function setAllSectionBodyMaxHeights(minHeight, isOnResize) {
     });
 
     $("body").css("overflow-y", bodyOverflowYValue);
+
 }
 
 $(function() {
 
+    var colourScheme = ColourScheme.DARK_PURPLE;
+
+    $("body").css({
+        "background-color": colourScheme.body,
+        "color": colourScheme.text
+    });
+
+    $(".section-title").each(function() {
+        $(this).focusin(function() {
+            $(this).css({
+                "outline": 0,
+                "font-weight": "bold"
+            });
+        });
+
+        $(this).focusout(function() {
+            $(this).css({
+                "font-weight": "normal"
+            });
+        });
+    });
+
+    $(".section-body").each(function() {
+        $(this).css({
+            "background-color": colourScheme.sectionBody
+        });
+    });
+
     $(".main-content").accordion({
         active: false,
+        beforeActivate: function(event, ui) {
+            colourScheme = setColourScheme(colourScheme);
+        },
         collapsible: true,
         header: ".section-title",
         heightStyle: "content"
@@ -45,4 +124,5 @@ $(function() {
     $(window).resize(function() {
         setAllSectionBodyMaxHeights(minSectionBodyMaxHeight, true);
     });
+
 });
