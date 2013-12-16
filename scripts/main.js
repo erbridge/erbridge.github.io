a98c3e443a69f342c8f651845e54bd67e4601d45
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2013-12-16 12:43:46 +0000

    Tidy up the javascript a little.

--- scripts/main.js
+++ scripts/main.js
@@ -1,170 +1,176 @@
-ColourScheme = {
-    DARK_GREEN: {
-        text: "#fff",
-        body: "#131",
-        sectionBody: "#353",
-        button: "#df0",
-        buttonText: "#000"
-    },
-    DARK_PURPLE: {
-        text: "#fff",
-        body: "#113",
-        sectionBody: "#335",
-        button: "#0df",
-        buttonText: "#000"
-    },
-    DARK_RED: {
-        text: "#fff",
-        body: "#311",
-        sectionBody: "#533",
-        button: "#fd0",
-        buttonText: "#000"
-    },
-    // LIGHT_GREEN: {
-    //     text: "#000",
-    //     body: "#585",
-    //     sectionBody: "#8b8"
-    // },
-    // LIGHT_PURPLE: {
-    //     text: "#000",
-    //     body: "#558",
-    //     sectionBody: "#88b"
-    // },
-    // LIGHT_RED: {
-    //     text: "#000",
-    //     body: "#855",
-    //     sectionBody: "#b88"
-    // }
-};
+/* global $, _ */
 
-function pickColourScheme() {
+(function() {
+    "use strict";
 
-    var colourSchemeNum   = _.size(ColourScheme);
-    var colourSchemeIndex = _.random(colourSchemeNum - 1);
-    var colourSchemeKey   = _.keys(ColourScheme)[colourSchemeIndex];
+    var ColourScheme = {
+        DARK_GREEN: {
+            text: "#fff",
+            body: "#131",
+            sectionBody: "#353",
+            button: "#df0",
+            buttonText: "#000"
+        },
+        DARK_PURPLE: {
+            text: "#fff",
+            body: "#113",
+            sectionBody: "#335",
+            button: "#0df",
+            buttonText: "#000"
+        },
+        DARK_RED: {
+            text: "#fff",
+            body: "#311",
+            sectionBody: "#533",
+            button: "#fd0",
+            buttonText: "#000"
+        },
+        // LIGHT_GREEN: {
+        //     text: "#000",
+        //     body: "#585",
+        //     sectionBody: "#8b8"
+        // },
+        // LIGHT_PURPLE: {
+        //     text: "#000",
+        //     body: "#558",
+        //     sectionBody: "#88b"
+        // },
+        // LIGHT_RED: {
+        //     text: "#000",
+        //     body: "#855",
+        //     sectionBody: "#b88"
+        // }
+    };
 
-    return ColourScheme[colourSchemeKey];
+    function pickColourScheme() {
 
-}
+        var colourSchemeNum   = _.size(ColourScheme);
+        var colourSchemeIndex = _.random(colourSchemeNum - 1);
+        var colourSchemeKey   = _.keys(ColourScheme)[colourSchemeIndex];
 
-function setColourScheme(currentColourScheme) {
+        return ColourScheme[colourSchemeKey];
 
-    var newColourScheme = pickColourScheme();
-
-    while(currentColourScheme === newColourScheme) {
-        newColourScheme = pickColourScheme();
     }
 
-    $("body").css({
-        "background-color": newColourScheme.body,
-        "color": newColourScheme.text
-    });
+    function setColourScheme(currentColourScheme) {
 
-    // FIXME: Animate this transition with the accordion.
-    $(".section-body-wrapper").each(function() {
-        $(this).css({
-            "background-color": newColourScheme.sectionBody
+        var newColourScheme = pickColourScheme();
+
+        while(currentColourScheme === newColourScheme) {
+            newColourScheme = pickColourScheme();
+        }
+
+        $("body").css({
+            "background-color": newColourScheme.body,
+            "color": newColourScheme.text
         });
-    });
 
-    $(".repo-button, .social-button, .email-button").css({
-        "background-color": newColourScheme.button,
-        "color": newColourScheme.buttonText
-    });
+        // FIXME: Animate this transition with the accordion.
+        $(".section-body-wrapper").each(function() {
+            $(this).css({
+                "background-color": newColourScheme.sectionBody
+            });
+        });
 
-    return newColourScheme;
+        $(".repo-button, .social-button, .email-button").css({
+            "background-color": newColourScheme.button,
+            "color": newColourScheme.buttonText
+        });
 
-}
+        return newColourScheme;
 
-function setAllSectionBodyMaxHeights(minMaxHeight, isOnResize) {
-
-    var bodyOverflowYValue   = "hidden";
-    var sectionBodyMaxHeight = $(window).height() - ($(".header").outerHeight(true));
-
-    if(!isOnResize) {
-        // FIXME: The header outerHeight seems to be off by 9 pixels during initial setup, but not during resizing.
-        sectionBodyMaxHeight -= 9;
     }
 
-    $(".section-title").each(function() {
-        sectionBodyMaxHeight -= $(this).outerHeight(true);
+    function setAllSectionBodyMaxHeights(minMaxHeight, isOnResize) {
+
+        var bodyOverflowYValue   = "hidden";
+        var sectionBodyMaxHeight = $(window).height() - ($(".header").outerHeight(true));
 
         if(!isOnResize) {
-            // FIXME: Each of these are off by 3 pixels.
-            sectionBodyMaxHeight -= 3;
+            // FIXME: The header outerHeight seems to be off by 9 pixels during initial setup, but not during resizing.
+            sectionBodyMaxHeight -= 9;
         }
-    });
 
-    if(sectionBodyMaxHeight < minMaxHeight) {
-        bodyOverflowYValue   = "auto";
-        sectionBodyMaxHeight = minMaxHeight;
+        $(".section-title").each(function() {
+            sectionBodyMaxHeight -= $(this).outerHeight(true);
+
+            if(!isOnResize) {
+                // FIXME: Each of these are off by 3 pixels.
+                sectionBodyMaxHeight -= 3;
+            }
+        });
+
+        if(sectionBodyMaxHeight < minMaxHeight) {
+            bodyOverflowYValue   = "auto";
+            sectionBodyMaxHeight = minMaxHeight;
+        }
+
+        $(".section-body-wrapper").each(function() {
+            $(this).css("max-height", sectionBodyMaxHeight);
+        });
+
+        $("body").css("overflow-y", bodyOverflowYValue);
+
     }
 
-    $(".section-body-wrapper").each(function() {
-        $(this).css("max-height", sectionBodyMaxHeight);
-    });
+    $(function() {
 
-    $("body").css("overflow-y", bodyOverflowYValue);
+        var colourScheme = ColourScheme.DARK_RED;
 
-}
-
-$(function() {
-
-    var colourScheme = ColourScheme.DARK_RED;
-
-    $("body").css({
-        "background-color": colourScheme.body,
-        "color": colourScheme.text
-    });
-
-    $("a").each(function() {
-        $(this).css({
+        $("body").css({
+            "background-color": colourScheme.body,
             "color": colourScheme.text
         });
-    });
 
-    $(".section-title").each(function() {
-        $(this).focusin(function() {
+        $("a").each(function() {
             $(this).css({
-                "outline": 0,
-                "font-weight": "bold"
+                "color": colourScheme.text
             });
         });
 
-        $(this).focusout(function() {
-            $(this).css({
-                "font-weight": "normal"
+        $(".section-title").each(function() {
+            $(this).focusin(function() {
+                $(this).css({
+                    "outline": 0,
+                    "font-weight": "bold"
+                });
+            });
+
+            $(this).focusout(function() {
+                $(this).css({
+                    "font-weight": "normal"
+                });
             });
         });
-    });
 
-    $(".section-body-wrapper").each(function() {
-        $(this).css({
-            "background-color": colourScheme.sectionBody
+        $(".section-body-wrapper").each(function() {
+            $(this).css({
+                "background-color": colourScheme.sectionBody
+            });
         });
+
+        $(".repo-button, .social-button, .email-button").css({
+            "background-color": colourScheme.button,
+            "color": colourScheme.buttonText
+        });
+
+        $(".main-content").accordion({
+            active: false,
+            beforeActivate: function() {
+                colourScheme = setColourScheme(colourScheme);
+            },
+            collapsible: true,
+            header: ".section-title",
+            heightStyle: "content"
+        });
+
+        var minSectionBodyMaxHeight = 100;
+
+        setAllSectionBodyMaxHeights(minSectionBodyMaxHeight, false);
+
+        $(window).resize(function() {
+            setAllSectionBodyMaxHeights(minSectionBodyMaxHeight, true);
+        });
+
     });
-
-    $(".repo-button, .social-button, .email-button").css({
-        "background-color": colourScheme.button,
-        "color": colourScheme.buttonText
-    });
-
-    $(".main-content").accordion({
-        active: false,
-        beforeActivate: function(event, ui) {
-            colourScheme = setColourScheme(colourScheme);
-        },
-        collapsible: true,
-        header: ".section-title",
-        heightStyle: "content"
-    });
-
-    var minSectionBodyMaxHeight = 100;
-
-    setAllSectionBodyMaxHeights(minSectionBodyMaxHeight, false);
-
-    $(window).resize(function() {
-        setAllSectionBodyMaxHeights(minSectionBodyMaxHeight, true);
-    });
-
-});
+})();
