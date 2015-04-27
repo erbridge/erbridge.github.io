f4eb3900bb57b90b72688608acd81a3300727a20
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2015-04-28 00:41:04 +0100

    Scroll the page to the top left before injecting the new content.

--- js/smooth.js
+++ js/smooth.js
@@ -2,7 +2,20 @@
 
 'use strict';
 
+var $body = $('body');
+
 $('#main').smoothState({
+  prefetch: true,
+  pageCacheSize: 5,
+  onStart: {
+    duration: 100,
+    render: function () {
+      $body.animate({
+        scrollTop: 0,
+        scrollLeft: 0,
+      }, 100);
+    },
+  },
   callback: function(url) {
     if (window.ga) {
       window.ga('send', 'pageview', window.location.pathname || url);
