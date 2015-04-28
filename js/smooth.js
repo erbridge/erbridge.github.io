70e0af7a718b7b3d4ac56e3486b3130814fff67e
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2015-04-28 01:42:59 +0100

    Animate the content height on page transition.

--- js/smooth.js
+++ js/smooth.js
@@ -4,18 +4,36 @@
 
 var $body = $('body');
 
-var content = $('#main').smoothState({
+var mainContent = $('#main').smoothState({
   prefetch: true,
   pageCacheSize: 5,
   onStart: {
     duration: 250,
     render: function () {
-      content.toggleAnimationClass('animation-scene--is-exiting');
+      mainContent.toggleAnimationClass('animation-scene--is-exiting');
       $body.animate({
         scrollTop: 0,
         scrollLeft: 0,
       }, 100);
     },
+  },
+  onEnd : {
+    duration: 100,
+    render: function (url, $container, $content) {
+      var oldPageContentHeight = $('.page-content').height();
+
+      $body.css('cursor', 'auto');
+      $body.find('a').css('cursor', 'auto');
+      $container.html($content);
+
+      var $pageContent = $('.page-content');
+
+      var newPageContentHeight = $pageContent.height();
+      $pageContent.height(oldPageContentHeight);
+      $pageContent.animate({
+        height: newPageContentHeight,
+      }, 100);
+    }
   },
   callback: function(url) {
     if (window.ga) {
