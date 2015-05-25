daeb95b6fc1b0ef6723bf65fa4bbc84dd5e131a0
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2015-05-25 01:10:52 +0100

    Disable the height animation.
    It breaks the layout on slow pages.

--- js/smooth.js
+++ js/smooth.js
@@ -4,36 +4,26 @@
 
 var $body = $('body');
 
-var mainContent = $('#main').smoothState({
+var $mainContent = $('#main').smoothState({
   prefetch: true,
   pageCacheSize: 5,
   onStart: {
     duration: 250,
-    render: function () {
-      mainContent.toggleAnimationClass('animation-scene--is-exiting');
+    render: function() {
+      $mainContent.toggleAnimationClass('animation-scene--is-exiting');
       $body.animate({
         scrollTop: 0,
         scrollLeft: 0,
       }, 100);
     },
   },
-  onEnd : {
+  onEnd: {
     duration: 100,
-    render: function (url, $container, $content) {
-      var oldPageContentHeight = $('.page-content').height();
-
+    render: function(url, $container, $content) {
       $body.css('cursor', 'auto');
       $body.find('a').css('cursor', 'auto');
       $container.html($content);
-
-      var $pageContent = $('.page-content');
-
-      var newPageContentHeight = $pageContent.height();
-      $pageContent.height(oldPageContentHeight);
-      $pageContent.animate({
-        height: newPageContentHeight,
-      }, 100);
-    }
+    },
   },
   callback: function(url) {
     if (window.ga) {
