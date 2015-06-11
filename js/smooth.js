6d664cbd7961cc2a70b878401404f009b4f0273a
Felix Laurie von Massenbach <felix@erbridge.co.uk>
2015-06-11 01:05:59 +0100

    Fix the spinner transition animations.

--- js/smooth.js
+++ js/smooth.js
@@ -31,29 +31,39 @@ var smoothState = $('#main').smoothState({
     duration: 0,
     render: function() {
       clearTimeout(spinnerTimeout);
+
       spinnerTimeout = setTimeout(function() {
-        $spinner.addClass('animation-scene--is-exiting');
+        $spinner.addClass('animation-scene--is-entering');
         $spinner.show();
       }, 250);
     },
   },
   onReady: {
-    duration: 250,
+    duration: 500,
     render: function($container, $content) {
       clearTimeout(spinnerTimeout);
-      $spinner.removeClass('animation-scene--is-exiting');
-      spinnerTimeout = setTimeout(function() {
-        $spinner.hide();
-      }, 100);
 
-      $container.removeClass('animation-scene--is-exiting');
-      $container.html($content);
+      var replaceContent = function() {
+        $container.removeClass('animation-scene--is-exiting');
+        $container.html($content);
+
+        $body.css('cursor', 'auto');
+        $body.find('a').css('cursor', 'auto');
+      };
+
+      if ($spinner.hasClass('animation-scene--is-entering')) {
+        $spinner.removeClass('animation-scene--is-entering');
+
+        spinnerTimeout = setTimeout(function() {
+          $spinner.hide();
+          replaceContent();
+        }, 250);
+      } else {
+        replaceContent();
+      }
     },
   },
   onAfter: function() {
-    $body.css('cursor', 'auto');
-    $body.find('a').css('cursor', 'auto');
-
     if (window.ga) {
       window.ga('send', 'pageview', window.location.pathname || smoothState.href);
     }
